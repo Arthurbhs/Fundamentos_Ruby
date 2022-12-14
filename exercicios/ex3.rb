01321e91b5c958f452b6b4c271d8b93af33dc629
arthur <arthur.shekinarfoxy@gmail.com>
2022-12-14 11:32:07 -0300

    feat: arrays/vetor and repeat  struture

---
+++ exercicios/ex3.rb
@@ -0,0 +1,15 @@
+#While em ruby
+
+i = 0
+ num = 5
+
+ while i < num do
+ puts "Contando... " + i.to_s
+ i += 1
+ end
+
+ #each estrutura de repetição simplificada
+
+ (0..5).each do |i|
+    puts "O valor lido foi: " + i.to_s
+    end
---
+++ exercicios/ex4.rb
@@ -0,0 +1,27 @@
+#formas de se declarar array
+v = [15,62,73,48]
+
+puts v[0]
+#ou...
+
+v1 = Array.new
+v1.push(15)
+ v1.push(62)
+
+
+ puts v1[1]
+
+ #tipo de variaveis mistas
+ v2 = [“curso”,62,1.4]
+bv.push(“hello”)
+
+#alinhando arrays em um unico array 
+
+v3 = [[11,12,13],[21,22,23],[31,32,33]]
+
+#correndo array usando each
+v3.each do |externo|
+     externo.each do |interno|
+    puts interno
+    end
+    end
